82b9f71066a8e4cd4c2b9945e242bb7ea2e597a8
hzx <1285180855@qq.com>
2020-11-23 22:25:20 +0800

    huangzx:初始化项目

---
+++ src/test/java/com/hzx/jenkinsdemo/JenkinsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hzx.jenkinsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
